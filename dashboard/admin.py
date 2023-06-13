b8ca2e84cf056b9c292fbfc5da035227e8468048
angel2708 <angeliyovalderrubio3@gmail.com>
2023-06-13 17:35:52 +0200

    Revert "proyecto"
    This reverts commit 607ce89865ffdcd5e9481d4384a6ac19c916dd97.

--- dashboard/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from dashboard.models import Function
-
-# Register your models here.
-
-admin.site.register(Function)
--- dashboard/functions/functions.py
+++
@@ -1,3 +0,0 @@
-def rawSerializer(cursor):
-    r = [dict((cursor.description[i][0], value) for i, value in enumerate(row)) for row in cursor.fetchall()]
-    return str(r).replace(": '",': "').replace("',",'",').replace(", '",', "').replace("':",'":').replace("'}",'"}').replace("{'",'{"').replace("None",'""').replace("True",'"1"').replace("False",'"0"').replace("\\xa0","")
